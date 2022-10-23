44c7cfb15b6d816f742d58e80104ac3c1617ee06
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-22 21:59:00 -0600

    + get car by id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Car } from './cars.types';
 
 @Controller('cars')
 export class CarsController {
+  private cars = [
+    { id: 1, make: 'Ford', model: 'F150', year: 2015 },
+    { id: 2, make: 'Chevy', model: 'Silverado', year: 2016 },
+    { id: 3, make: 'Dodge', model: 'Ram', year: 2017 },
+  ];
   @Get()
-  findAll(): string[] {
-    return ['Ford', 'Chevy'];
+  findAll(): Car[] {
+    return this.cars;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Car {
+    return this.cars.find((car) => car.id === +id);
   }
 }
